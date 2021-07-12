1505e8e902413eca9e2f81424de96506d3e60035
BondOfSisters <78360829+BondOfSisters@users.noreply.github.com>
2021-07-12 11:13:46 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,36 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    
+    stroke("BLACK")
+    strokeWeight(3)
+    fill("RED")
+    textSize(20)
+    text("R E V B A I L A", 100,50)
+    fill("YELLOW")
+    text("Hint: ALWAYS CHANGING,NOT CONSTANT !!", 100,70)
+
+    stroke("BLACK")
+    strokeWeight(3)
+    fill("RED")
+    textSize(20)
+    text("I O N T C N F U ", 700,150)
+    fill("YELLOW")
+    text("Hint: PERFORMS A PARTICULAR TASK !!", 600,170)
+
+    stroke("BLACK")
+    strokeWeight(2)
+    fill("RED")
+    textSize(20)
+    text("D T B S A A A E ", 100,310)
+    fill("YELLOW")
+    text("Hint: STORES ALL INFORMATION !!", 100,330)
+
+
+
+    
+
+}
